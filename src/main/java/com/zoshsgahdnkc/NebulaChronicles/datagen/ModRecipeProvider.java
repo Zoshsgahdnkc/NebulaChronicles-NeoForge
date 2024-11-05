20e3a3a24eb2c7988fc17e16cb7f84526b5b5a15
Zoshsgahdnkc <2987901713@qq.com>
2024-11-05 13:17:01 +0800

    recipe dg

--- src/main/java/com/zoshsgahdnkc/NebulaChronicles/datagen/ModRecipeProvider.java
+++ src/main/java/com/zoshsgahdnkc/NebulaChronicles/datagen/ModRecipeProvider.java
@@ -1,17 +1,24 @@
 package com.zoshsgahdnkc.NebulaChronicles.datagen;
 
 import com.zoshsgahdnkc.NebulaChronicles.NebulaChronicles;
+import com.zoshsgahdnkc.NebulaChronicles.datagen.tags.ModItemTags;
 import com.zoshsgahdnkc.NebulaChronicles.registries.ModBlocks;
 import com.zoshsgahdnkc.NebulaChronicles.registries.ModItems;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.*;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.crafting.StonecutterRecipe;
+import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.neoforged.neoforge.common.Tags;
 import net.neoforged.neoforge.common.conditions.IConditionBuilder;
+import net.neoforged.neoforge.common.data.internal.NeoForgeBlockTagsProvider;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredItem;
 import org.jetbrains.annotations.NotNull;
@@ -24,6 +31,26 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
     }
     @Override
     protected void buildRecipes(@NotNull RecipeOutput output) {
+        // Main
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.AETHER_ROOT)
+                .define('#', ModItems.AETHER_ROOT_SPORE)
+                .pattern("###")
+                .pattern("###")
+                .pattern("###")
+                .group(null)
+                .unlockedBy(getHasName(ModItems.AETHER_ROOT_SPORE), has(ModItems.AETHER_ROOT_SPORE))
+                .save(output, ResourceLocation.fromNamespaceAndPath(NebulaChronicles.MODID, getSimpleRecipeName(ModBlocks.AETHER_ROOT)));
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.VACUUM_TUBE)
+                .define('#', ModItems.NICKELSTEEL_PLASTIC)
+                .define('0', Tags.Items.GLASS_BLOCKS)
+                .pattern(" 0#")
+                .pattern("0 0")
+                .pattern("#0 ")
+                .group(null)
+                .unlockedBy(getHasName(ModItems.NICKELSTEEL_PLASTIC), has(ModItems.NICKELSTEEL_PLASTIC))
+                .save(output);
+        stonecutterResultFromBase(output, RecipeCategory.MISC, ModItems.CALCITE_CASING, Blocks.CALCITE);
+
         // Astral Kitchen
         ShapelessRecipeBuilder.shapeless(RecipeCategory.FOOD, ModItems.WHITE_BUD_STEW)
                 .requires(Items.BOWL)
@@ -34,14 +61,6 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
                 .unlockedBy(getHasName(ModItems.WHITE_BUD_LEAVES), has(ModItems.WHITE_BUD_LEAVES))
                 .unlockedBy(getHasName(ModItems.AETHER_ROOT_SPORE), has(ModItems.AETHER_ROOT_SPORE))
                 .save(output);
-        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.AETHER_ROOT)
-                .define('#', ModItems.AETHER_ROOT_SPORE)
-                .pattern("###")
-                .pattern("###")
-                .pattern("###")
-                .group(null)
-                .unlockedBy(getHasName(ModItems.AETHER_ROOT_SPORE), has(ModItems.AETHER_ROOT_SPORE))
-                .save(output, ResourceLocation.fromNamespaceAndPath(NebulaChronicles.MODID, getSimpleRecipeName(ModBlocks.AETHER_ROOT)));
 
         // Building Blocks
         nineBlockStorageRecipes(output, RecipeCategory.MISC, ModItems.WHITE_BUD_LEAVES, RecipeCategory.BUILDING_BLOCKS, ModBlocks.WHITE_BUD_LEAVES_BLOCK);
@@ -60,6 +79,34 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
                 .unlockedBy(getHasName(ModBlocks.SILVERBLANC_STONE_BRICKS_SLAB), has(ModBlocks.SILVERBLANC_STONE_BRICKS_SLAB))
                 .save(output);
         stonecutterResultFromBase(output, RecipeCategory.BUILDING_BLOCKS, ModBlocks.CHISELED_SILVERBLANC_STONE_BRICKS, ModBlocks.SILVERBLANC_STONE_BRICKS);
+        ShapedRecipeBuilder.shaped(RecipeCategory.BUILDING_BLOCKS, ModBlocks.FORTRESS_BLOCK, 24)
+                .define('N', ModItems.NICKELSTEEL_PLASTIC)
+                .define('I', Items.IRON_INGOT)
+                .pattern("NI")
+                .pattern("IN")
+                .group(null)
+                .unlockedBy(getHasName(ModItems.NICKELSTEEL_PLASTIC), has(ModItems.NICKELSTEEL_PLASTIC))
+                .save(output);
+        stonecutterResultFromBase(output, RecipeCategory.BUILDING_BLOCKS, ModBlocks.FORTRESS_WALL, ModBlocks.FORTRESS_BLOCK);
+        stonecutterResultFromBase(output, RecipeCategory.BUILDING_BLOCKS, ModBlocks.FORTRESS_WALL_LIGHT_UNLIT, ModBlocks.FORTRESS_BLOCK);
+        stonecutterResultFromBase(output, RecipeCategory.BUILDING_BLOCKS, ModBlocks.FORTRESS_DOOR, ModBlocks.FORTRESS_BLOCK);
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.BUILDING_BLOCKS, ModBlocks.FORTRESS_WALL_LIGHT)
+                .requires(ModBlocks.FORTRESS_WALL_LIGHT_UNLIT)
+                .requires(Tags.Items.DUSTS_GLOWSTONE)
+                .unlockedBy(getHasName(ModBlocks.FORTRESS_WALL_LIGHT_UNLIT), has(ModBlocks.FORTRESS_WALL_LIGHT_UNLIT))
+                .save(output);
+        stonecutterResultFromBase(output, RecipeCategory.BUILDING_BLOCKS, ModBlocks.IRON_BRICKS, Items.IRON_INGOT, 8);
+        stonecutterResultFromBase(output, RecipeCategory.BUILDING_BLOCKS, ModBlocks.IRON_BRICKS_SLAB, Items.IRON_INGOT, 16);
+        stonecutterResultFromBase(output, RecipeCategory.BUILDING_BLOCKS, ModBlocks.IRON_BRICKS_STAIRS, Items.IRON_INGOT, 8);
+        stonecutterResultFromBase(output, RecipeCategory.BUILDING_BLOCKS, ModBlocks.IRON_COLLAGE, Items.IRON_INGOT, 8);
+        stonecutterResultFromBase(output, RecipeCategory.BUILDING_BLOCKS, ModBlocks.BUNKER_BRICKS, Items.IRON_INGOT, 8);
+        stonecutterResultFromBase(output, RecipeCategory.BUILDING_BLOCKS, ModBlocks.LIGHT_TILE, Items.IRON_INGOT, 8);
+        stonecutterResultFromBase(output, RecipeCategory.BUILDING_BLOCKS, ModBlocks.DARK_TILE, Items.IRON_INGOT, 8);
+        stonecutterResultFromBase(output, RecipeCategory.BUILDING_BLOCKS, ModBlocks.SIMPLE_VAULT_STAIRS, Items.IRON_INGOT, 8);
+        stonecutterResultFromBase(output, RecipeCategory.BUILDING_BLOCKS, ModBlocks.THICK_VAULT_STAIRS, Items.IRON_INGOT, 4);
+
+
+
         // Block Variants
         slabAndStair(output, ModBlocks.IRON_BRICKS, ModBlocks.IRON_BRICKS_SLAB, ModBlocks.IRON_BRICKS_STAIRS);
         slabAndStair(output, ModBlocks.SILVERBLANC_STONE, ModBlocks.SILVERBLANC_STONE_SLAB, ModBlocks.SILVERBLANC_STONE_STAIRS);
@@ -84,6 +131,14 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
         createIngotRecipes(output, ModItems.THULIUM_188_NUGGET, ModItems.THULIUM_188_INGOT, ModBlocks.THULIUM_188_BLOCK);
         createIngotRecipes(output, ModItems.ULTRALLOY_NUGGET, ModItems.ULTRALLOY_INGOT, ModBlocks.ULTRALLOY_BLOCK);
         createIngotRecipes(output, ModItems.LEMURIUM_NUGGET, ModItems.LEMURIUM_INGOT, ModBlocks.LEMURIUM_BLOCK);
+
+        SimpleCookingRecipeBuilder.blasting(Ingredient.of(ModItemTags.SMELTS_TO_NICKEL), RecipeCategory.MISC, ModItems.NICKEL_INGOT, 0.7f, 100).unlockedBy("has_smelts_to_nickel", has(ModItemTags.SMELTS_TO_NICKEL)).save(output, getBlastingRecipeName(ModItems.NICKEL_INGOT));
+        SimpleCookingRecipeBuilder.smelting(Ingredient.of(ModItemTags.SMELTS_TO_NICKEL), RecipeCategory.MISC, ModItems.NICKEL_INGOT, 0.7f, 200).unlockedBy("has_smelts_to_nickel", has(ModItemTags.SMELTS_TO_NICKEL)).save(output, getSmeltingRecipeName(ModItems.NICKEL_INGOT));
+        smeltingOrBlasting(output, RecipeCategory.MISC, ModBlocks.SILVERBLANC_COPPER_ORE, Items.COPPER_INGOT, 0.7f, 100);
+        smeltingOrBlasting(output, RecipeCategory.MISC, ModBlocks.SILVERBLANC_IRON_ORE, Items.IRON_INGOT, 0.7f, 100);
+        smeltingOrBlasting(output, RecipeCategory.MISC, ModBlocks.SILVERBLANC_GOLD_ORE, Items.GOLD_INGOT, 1f, 100);
+        smeltingOrBlasting(output, RecipeCategory.MISC, ModBlocks.SILVERBLANC_LAPIS_ORE, Items.LAPIS_LAZULI, 0.2f, 100);
+        smeltingOrBlasting(output, RecipeCategory.MISC, ModBlocks.SILVERBLANC_DIAMOND_ORE, Items.DIAMOND, 1f, 100);
     }
 
     protected static void createIngotRecipes(RecipeOutput output,
@@ -104,6 +159,11 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
         stairBuilder(stair, Ingredient.of(block)).unlockedBy(getHasName(block), has(block)).save(output);
         stonecutterResultFromBase(output, RecipeCategory.BUILDING_BLOCKS, slab, block, 2);
         stonecutterResultFromBase(output, RecipeCategory.BUILDING_BLOCKS, stair, block);
+    }
+
+    protected static void smeltingOrBlasting(RecipeOutput output, RecipeCategory category, ItemLike from, ItemLike to, float exp, int blastingTime) {
+        SimpleCookingRecipeBuilder.blasting(Ingredient.of(from), category, to, exp, blastingTime).unlockedBy(getHasName(from), has(from)).save(output, getBlastingRecipeName(to));
+        SimpleCookingRecipeBuilder.smelting(Ingredient.of(from), category, to, exp, blastingTime * 2).unlockedBy(getHasName(from), has(from)).save(output, getSmeltingRecipeName(to));
     }
 
 }
--- src/main/java/com/zoshsgahdnkc/NebulaChronicles/datagen/tags/ModItemTags.java
+++ src/main/java/com/zoshsgahdnkc/NebulaChronicles/datagen/tags/ModItemTags.java
@@ -2,6 +2,7 @@ package com.zoshsgahdnkc.NebulaChronicles.datagen.tags;
 
 import com.zoshsgahdnkc.NebulaChronicles.NebulaChronicles;
 import com.zoshsgahdnkc.NebulaChronicles.registries.ModBlocks;
+import com.zoshsgahdnkc.NebulaChronicles.registries.ModItems;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
@@ -12,6 +13,7 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.registries.DeferredBlock;
+import org.apache.http.cookie.SM;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -23,6 +25,7 @@ public class ModItemTags extends ItemTagsProvider {
         super(output, lookupProvider, blockTagProvider, NebulaChronicles.MODID, existingFileHelper);
     }
     public static final TagKey<Item> METAL_SCAFFOLDING = TagKey.create(Registries.ITEM, getRL("metal_scaffolding"));
+    public static final TagKey<Item> SMELTS_TO_NICKEL = TagKey.create(Registries.ITEM, getRL("smelts_to_nickel"));
 
     private static ResourceLocation getRL(String name) {
         return ResourceLocation.fromNamespaceAndPath(NebulaChronicles.MODID, name);
@@ -33,6 +36,11 @@ public class ModItemTags extends ItemTagsProvider {
         tag(METAL_SCAFFOLDING)
                 .add(ModBlocks.IRON_SCAFFOLDING.asItem())
                 .add(ModBlocks.NICKELSTEEL_PLASTIC_SCAFFOLDING.asItem());
+        tag(SMELTS_TO_NICKEL)
+                .add(ModItems.RAW_NICKEL.get())
+                .add(ModBlocks.NICKEL_ORE.asItem())
+                .add(ModBlocks.DEEPSLATE_NICKEL_ORE.asItem())
+                .add(ModBlocks.SILVERBLANC_NICKEL_ORE.asItem());
     }
 
     private ResourceLocation rl(DeferredBlock<Block> block) {
